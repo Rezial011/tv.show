3ff994c47dd34957b09a22c9e0d77b177bbcf1b3
Rezial011 <dafaalfarezi27@gmail.com>
2025-04-06 20:10:02 +0700

    fix vercel routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { React } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import './index.css'
 import HomeLayout from './components/home/HomeLayout'
 import SinglePage from './components/singlePage/SinglePage'
@@ -8,12 +8,12 @@ function App() {
 
   return (
     <>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomeLayout />} />
         <Route path="/movie/:id" element={<SinglePage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
     </>
   )
 }
